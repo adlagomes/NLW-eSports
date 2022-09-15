8dc8cb5655a15a73ad6bb2d2056ff0a4a7f1996c
Adla Gomes <adlagomes@gmail.com>
2022-09-15 18:06:40 -0300

    edit files

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/main.css';
+
+function App() {
+
+  return (
+    <div className="max-w-[1344px] mx-auto flex items-center flex-col my-20">
+      <img src="./logo-nlw-esports.svg" alt="" />
+      <h1 className='text-6xl text-white font-black mt-20'>Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo </span>está aqui!</h1>
+    </div>
+  )
+}
+
+export default App
