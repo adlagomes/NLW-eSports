219779c42e7d4f1c8cb2d76c13dedb9f833bc48b
Adla Gomes <adlagomes@gmail.com>
2022-09-16 14:48:51 -0300

    finish edit css

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,93 @@
-import './styles/main.css';
+import {MagnifyingGlassPlus} from 'phosphor-react'
+import "./styles/main.css";
 
 function App() {
-
   return (
     <div className="max-w-[1344px] mx-auto flex items-center flex-col my-20">
       <img src="./logo-nlw-esports.svg" alt="" />
-      <h1 className='text-6xl text-white font-black mt-20'>Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo </span>está aqui!</h1>
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{" "}
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          duo{" "}
+        </span>
+        está aqui!
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-1.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 letf-0 right-0">
+            <strong className="font-bold text-white block">Nome do Jogo</strong>
+
+            <span className="text-zinc-300 text-sm block">Anúncio</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-2.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 letf-0 right-0">
+            <strong className="font-bold text-white block">Nome do Jogo</strong>
+
+            <span className="text-zinc-300 text-sm block">Anúncio</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-3.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 letf-0 right-0">
+            <strong className="font-bold text-white block">Nome do Jogo</strong>
+
+            <span className="text-zinc-300 text-sm block">Anúncio</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-4.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 letf-0 right-0">
+            <strong className="font-bold text-white block">Nome do Jogo</strong>
+
+            <span className="text-zinc-300 text-sm block">Anúncio</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-5.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 letf-0 right-0">
+            <strong className="font-bold text-white block">Nome do Jogo</strong>
+
+            <span className="text-zinc-300 text-sm block">Anúncio</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-6.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 letf-0 right-0">
+            <strong className="font-bold text-white block">Nome do Jogo</strong>
+
+            <span className="text-zinc-300 text-sm block">Anúncio</span>
+          </div>
+        </a>
+      </div>
+
+      <div className="pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden">
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-white text-2xl font-black block">
+              Não encontrou seu duo?
+            </strong>
+            <span className="text-zinc-400 block">
+              Publique um anúncio para encontrar novos players!
+            </span>
+          </div>
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24}/>
+            Publicar anúncio
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
